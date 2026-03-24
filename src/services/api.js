f5006ff6b251823c426e6ed014c382aca0bb0ec9
Rakesh Pal <rpal86530@gmail.com>
2026-03-24 18:14:50 +0530

    update api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8082/api",
+ baseURL: "https://easyticket-backend-1.onrender.com/api",
 });
 
 API.interceptors.request.use(
